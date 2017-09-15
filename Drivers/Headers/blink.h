db27ce5ab15617d9edc2871327a8f5615737319d
Luke Barbier <lukebarbier12@gmail.com>
2017-09-15 07:43:45 -0600

    Added blink driver

--- Drivers/Headers/blink.h
+++ Drivers/Headers/blink.h
@@ -8,6 +8,38 @@
 #ifndef DRIVERS_HEADERS_BLINK_H_
 #define DRIVERS_HEADERS_BLINK_H_
 
+#include "button.h"
+#include <stdint.h>
 
+typedef enum LED_t{
+    LED1,
+    LED2,
+    LED1_LED2
+}LED;
+
+typedef enum COLOR_t{
+    RED,
+    GREEN,
+    BLUE
+}COLOR;
+
+/* Sets the color of LED2 (0-7) */
+void setColorLED2(COLOR col);
+
+/* Toggles LED 1 or 2 at specified rate using timer intrpt*/
+void blinkRate(LED led, COLOR col, uint8_t milli_period);
+
+/* Toggles LED 1 with button depression of inputted button
+ * LED1 is always COLOR = RED */
+void blinkPushLED1(BUTTON but);
+
+/* Cycles through 7 colors of led */
+void blinkCycle(BUTTON but);
+
+/* Configures led pin(s) as output(s) set to low */
+void _configLED(LED led);
+
+/* Toggles the LED */
+void toggleLED1();
 
 #endif /* DRIVERS_HEADERS_BLINK_H_ */
--- Drivers/blink.c
+++ Drivers/blink.c
@@ -3,6 +3,69 @@
  *
  *  Created on: Sep 15, 2017
  *      Author: luke
+ *      MSP's LED Driver File
+ *      See Header File for function descriptions
  */
 
 
+#include "msp.h"
+#include "Headers/blink.h"
+#include "Headers/button.h"
+#include "Headers/timerA.h"
+
+void blinkRate(LED led, COLOR col, uint16_t milli_period)
+{
+    if(led == LED1)                     // always color red
+    {
+        _configLED(LED1);
+        configTimerA(TIMERA_0, milli_period, &toggleLED1);
+    }
+    else
+    {
+
+    }
+}
+
+void blinkPushLED1(BUTTON but)
+{
+    _configLED(LED1_LED2);
+    P1OUT |= BIT0;              // led on
+    configButton(but, &toggleLED1);
+}
+
+
+void _configLED(LED led)
+{
+    switch(led)
+    {
+    case LED1:
+        P1DIR |= BIT0;          // set as output
+        P1OUT &= ~BIT0;         // set 1.0 low
+        break;
+    case LED2:
+        P2DIR |= BIT0;
+        P2DIR |= BIT1;
+        P2DIR |= BIT2;
+        P2OUT &= ~BIT0;         // set 2.0 low
+        P2OUT &= ~BIT1;         // set 2.1 low
+        P2OUT &= ~BIT2;         // set 2.2 low
+        break;
+    case LED1_LED2:
+        P1DIR |= BIT0;
+        P1OUT &= ~BIT0;
+        P2DIR |= BIT0;
+        P2DIR |= BIT1;
+        P2DIR |= BIT2;
+        P2OUT &= ~BIT0;
+        P2OUT &= ~BIT1;
+        P2OUT &= ~BIT2;
+        break;
+    default: // bad input
+        break;
+    }
+}
+
+void toggleLED1()
+{
+    P1OUT ^= BIT0;
+}
