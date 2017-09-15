8dbc70635d571a07dc04592d7c791b61b5d6a8d5
Luke Barbier <lukebarbier12@gmail.com>
2017-09-15 07:45:06 -0600

    Added button driver

--- Drivers/Headers/button.h
+++ Drivers/Headers/button.h
@@ -8,6 +8,31 @@
 #ifndef DRIVERS_HEADERS_BUTTON_H_
 #define DRIVERS_HEADERS_BUTTON_H_
 
+uint8_t buttons_in_use;     // determine if a timerA is already in use
 
+/* global function ptr's for intrpts */
+void (*button_s1_handler)(void);
+void (*button_s2_handler)(void);
+
+typedef enum BUTTON_t{
+    BUTTON_S1,
+    BUTTON_S2
+}BUTTON;
+
+typedef enum BUTTON_CONFIG_t{
+    BUTTON_CONFIG_NO_ERROR,
+    BUTTON_CONFIG_ERR_IN_USE,
+    BUTTON_CONFIG_ERR_NO_INIT,
+    BUTTON_CONFIG_ERR_BAD_INPUT
+}BUTTON_CONFIG;
+
+/* Initializes button module for use. Must be called before all other button functions */
+void initButtons(void);
+
+/* Configures button to trigger an interrupt and call inputed function */
+BUTTON_CONFIG configButton(BUTTON but, void(*handler)(void));
+
+/* Port 1 ISR */
+void Port1Handler(void);
 
 #endif /* DRIVERS_HEADERS_BUTTON_H_ */
--- Drivers/button.c
+++ Drivers/button.c
@@ -5,4 +5,54 @@
  *      Author: luke
  */
 
+#include "msp.h"
+#include "Headers/button.h"
+#include <stdio.h> // NULL
 
+void initButtons()
+{
+    buttons_in_use = 0;
+    button_s1_handler = NULL;
+    button_s2_handler = NULL;
+}
+
+BUTTON_CONFIG configButton(BUTTON but, void(*handler)(void))
+{
+    switch(but)
+    {
+    case BUTTON_S1:
+        if(buttons_in_use & 1)
+            return BUTTON_CONFIG_ERR_IN_USE;
+        else
+            buttons_in_use |= 1;
+        P1DIR &= ~BIT1;
+        P1IE |= BIT1;
+        P1IFG &= ~BIT1;
+        P1IES |= BIT1;                  // LOW TO HIGH flag intrpt
+        button_s1_handler = handler;
+        P1REN |= BIT1;
+        P1OUT |= BIT1;                  // PULL UP
+
+        NVIC_EnableIRQ(PORT1_IRQn);
+
+        break;
+    case BUTTON_S2:
+        if(buttons_in_use & 1)
+            return BUTTON_CONFIG_ERR_IN_USE;
+        else
+            buttons_in_use |= 2;
+
+        button_s2_handler = handler;
+        break;
+    default:
+        return BUTTON_CONFIG_ERR_BAD_INPUT;
+    }
+    __enable_interrupt();
+    return BUTTON_CONFIG_NO_ERROR;
+}
+
+void Port1Handler(void)
+{
+    (*button_s1_handler)();
+    P1IFG &= ~BIT1;
+}
