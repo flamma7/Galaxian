9a0b9604ff27f6d38f1fb7b64e30c367c429c3f2
Luke Barbier <lukebarbier12@gmail.com>
2017-09-15 07:47:20 -0600

    Added spi driver

--- Drivers/Headers/spi.h
+++ Drivers/Headers/spi.h
@@ -8,6 +8,37 @@
 #ifndef DRIVERS_HEADERS_SPI_H_
 #define DRIVERS_HEADERS_SPI_H_
 
+/* Crystalfontz CFAF128128B-0145T*/
+    // 20 FPS
+    // 262K colors, contrast 350
+#include <stdint.h>
 
+/* Configures pins 4.1(STE), 1.5(SCK), 1.6(MOSI), 1.7(MISO)
+ * for SPI mode using UCB0CTLW0 */
+void configure_SPI_p14_p15_p16_p17_UCB0CTLW0(void);
+void _configure_SPI_p14_p15_p16_p17_UCB0CTLW0_pins(void);
+
+uint8_t read_spi(uint8_t reg);
+void write_spi(uint8_t reg, uint8_t data);
+
+/* Make a begin transaction and end transaction function?? */
+/* Or simply a send and receive thing */
+
+
+/* J1.7 LCD SPI clock = p1.5
+ * J2.13 LCD SPI chip select = p5.0
+ * J2.15 LCD SPI MOSI = p1.6
+ * J4.31 LCD reset pin = p3.7
+ * J4.39 LCD backlight
+ *    multiplexed with RGB LED through J5..?
+ *    */
+
+/* Receive and transmit operations operate concurrently
+ * I'll probably need to work the slave select myself
+ * Questinons:
+ * 7-bit or 8-bit data len?
+ * LSB or MSB first
+ * 3-pin or 4pin
+ */
 
 #endif /* DRIVERS_HEADERS_SPI_H_ */
--- Drivers/spi.c
+++ Drivers/spi.c
@@ -5,4 +5,86 @@
  *      Author: luke
  */
 
+#include "Headers/spi.h"
+#include "msp.h"
+#include <stdio.h>
 
+
+void configure_SPI_p14_p15_p16_p17_UCB0CTLW0(void)
+{
+    UCB0CTLW0 |= UCSWRST;   // software reset
+    UCB0CTLW0 |= UCCKPH;    // data changed on first UCLK edge?
+    UCB0CTLW0 |= UCCKPL;    // clock polarity inactive high
+    UCB0CTLW0 |= UCMSB;     // MSB first
+    UCB0CTLW0 &= ~UC7BIT;   // 8-bit data
+    UCB0CTLW0 |= UCMST;     // master mode
+    UCB0CTLW0 |= UCMODE_0;  // 3-pin, p1.4(UCB0STE) doesn't have pin on MSP
+    UCB0CTLW0 |= UCSYNC;    // Synchronous mode
+    UCB0CTLW0 |= UCSSEL_3;  // SMCLK clock (3MHz)
+    UCB0BRW = 4;            // 4 Bit clock prescalar
+    //UCB0CTLW0 |= UCSTEM;  // STE pin used in 4-wire slave
+    UCB0CTLW0 &= ~UCSWRST;  // register open for operation
+
+    _configure_SPI_p14_p15_p16_p17_UCB0CTLW0_pins();
+}
+
+void _configure_SPI_p14_p15_p16_p17_UCB0CTLW0_pins(void)
+{
+    // page 139 of MSP432p401r_Overview.pdf
+//  P1SEL1 &= ~BIT4;
+//  P1SEL0 |= BIT4;     UCB0STE p1.4 doesn't have pin on MSP
+
+    // New SS p4.1
+    P4DIR |= BIT1;      // OUTPUT
+    P4SEL1 &= ~BIT1;    // I/O mode
+    P4SEL0 &= ~BIT1;
+    P4OUT |= BIT1;      // Active low, start high
+
+    P1SEL1 &= ~BIT5;
+    P1SEL0 |= BIT5;
+
+    P1SEL1 &= ~BIT6;
+    P1SEL0 |= BIT6;
+
+    P1SEL1 &= ~BIT7;
+    P1SEL0 |= BIT7;
+}
+
+void write_spi(uint8_t reg, uint8_t data)
+{
+    P4OUT &= ~BIT1;                 // SS
+    UCB0TXBUF = reg;
+    while(!(UCB0IFG & UCTXIFG));    // wait for the data to be sent
+    UCB0TXBUF = data;
+    P4OUT |= BIT1;
+}
+
+
+/* When UCB0RXBUF has not been updated, it defaults to 0xFF */
+uint8_t read_spi(uint8_t reg)
+{
+    P4OUT &= ~BIT1;                 // SS
+    UCB0TXBUF = reg;
+    while(!(UCB0IFG & UCRXIFG));    // wait for the data to be received
+    UCB0TXBUF = 0xff;
+    uint8_t data = UCB0RXBUF;
+    printf("SPI Data1: %x\n", data);
+    while(!(UCB0IFG & UCRXIFG));
+    UCB0TXBUF = 0xff;
+    while(!(UCB0IFG & UCRXIFG));
+    data = UCB0RXBUF;
+    printf("SPI Data2: %x\n", data);
+    P4OUT |= BIT1;
+    return data;
+}
+
+
+
+/* TODO
+ *  1) Locate pins and make a spi library
+ *  2) Make a adc read library
+ *  3) Start making a CFAF128128B display library
+ *  4) Create a state diagram of game
+ *  5) Create data structures for game
+ *  6) Create graphics for game
+ *  7) Start putting it together->modular as possible of course ;)*/
